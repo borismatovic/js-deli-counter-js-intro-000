54816fed73a935fa1aa6acca309458a3cfb9c0b6
confident-history-3187 <boris.matovic@gmail.com>
2019-11-30 20:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeliLine = [];
-function takeANumber(arr, name){
+function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   console.log(`Welcome, ${name}. You are number ${arr.length + 1} in line.`)
 }
