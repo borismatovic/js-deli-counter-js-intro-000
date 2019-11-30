36271cdbba99dd7df0e1e4c198594945720659a8
confident-history-3187 <boris.matovic@gmail.com>
2019-11-30 20:38:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(arr, name){
   arr.push(name)
   return `Welcome, ${name}. You are number ${arr.length} in line.`
 }
+
+function nowServing(arr){
+  return arr.length === 0 ? 'Line is empty.' : 'Line is not empty.'
+}
