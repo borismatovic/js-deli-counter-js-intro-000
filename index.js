f313b050d3fdb4eba7f67589e045ae13f9e4fbfc
confident-history-3187 <boris.matovic@gmail.com>
2019-11-30 20:35:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(arr, name){
   arr.push(name)
-  return `Welcome, ${name}. You are number ${arr.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${arr.length} in line.`
 }
